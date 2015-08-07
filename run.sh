647f2336e18db8588fd05909d342e678e61af553
Al Maline <amaline@users.noreply.github.com>
2015-08-07 14:58:07 -0400

    temp solution to background hook.js

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-#npm install git+https://github.com/amaline/cu-bot-scripts.git
+GITHUB_SECRET=$GITHUB_SECRET node hook.js 2>&1 > hook.log &
 HUBOT_SLACK_TOKEN=$HUBOT_SLACK_TOKEN ./bin/hubot --adapter slack --alias !
